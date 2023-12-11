04fa0d4669d1358bbc3bba51341b7dde8bd45c92
Mhirii <ahmedmhiri218@gmail.com>
2023-12-11 22:20:01 +0100

    docstring

--- helpers/init.lua
+++ helpers/init.lua
@@ -1,8 +1,24 @@
 local sysctrl = require("helpers.system_control")
 local make    = require("helpers.make")
 
+
+local function get_next_screen(direction)
+  local previous_direction = direction == "right" and "left" or "right"
+  local screen = awful.screen.focused()
+  if (screen.get_next_in_direction(screen, direction)) then
+    return screen.get_next_in_direction(screen, direction)
+  end
+
+  while screen.get_next_in_direction(screen, previous_direction) ~= nil do
+    screen = screen.get_next_in_direction(screen, previous_direction)
+  end
+
+  return screen
+end
+
 local helpers = {
   system_control = sysctrl,
-  make = make
+  make = make,
+  get_next_screen = get_next_screen
 }
 return helpers
--- helpers/system_control.lua
+++ helpers/system_control.lua
@@ -4,6 +4,19 @@ local gears_string = require("gears.string")
 
 local syscontrol = {}
 -- ───────────────────────────── Volume control ───────────────────────────── --
+
+--[[
+  Retrieves the current volume level and mute status using the `pamixer` command.
+
+  Example Usage:
+    syscontrol.get_volume()
+
+  Inputs:
+    None
+
+  Outputs:
+    None. The function emits a signal with the volume level and mute status.
+  ]]
 function syscontrol.get_volume()
   spawn.easy_async(
     "pamixer --get-mute --get-volume", function(stdout)
@@ -13,6 +26,22 @@ function syscontrol.get_volume()
   )
 end
 
+--[[ Controls the volume level.
+
+  Args:
+
+    type (string): The action to be performed. Possible values are "increase", "decrease", "mute", or any other value to set the volume directly.
+
+    value (number): The amount by which the volume should be changed. This parameter is only used when `type` is "increase" or "decrease".
+
+  Example Usage:
+
+    syscontrol.volume_control("increase", 10)
+    `This code will increase the volume by 10 units.`
+  ]]
+---@param type string
+---@param value number
+---@return nil
 function syscontrol.volume_control(type, value)
   local cmd
   if type == "increase" then
@@ -25,10 +54,19 @@ function syscontrol.volume_control(type, value)
     cmd = "pamixer --set-volume " .. tostring(value)
   end
 
-  spawn.easy_async(cmd, get_volume)
+  spawn.easy_async(cmd, syscontrol.get_volume)
 end
 
 -- ─────────────────────────── Brightness control ─────────────────────────── --
+
+--[[ Returns the current brightness level.
+
+  Example Usage:
+
+    syscontrol.get_brightness()
+    `This code will return the current brightness level.`
+  ]]
+---@return nil
 function syscontrol.get_brightness()
   spawn.easy_async_with_shell(
     "brightnessctl i | grep -o '[0-9]*%'", function(stdout)
@@ -38,6 +76,28 @@ function syscontrol.get_brightness()
   )
 end
 
+--[[ Controls the screen brightness level.
+
+ Args:
+
+   type (string): The action to be performed. Possible values are "increase" or "decrease" to adjust brightness by 5%, or any other value to set the brightness directly.
+
+   value (number): The brightness percentage to set when `type` is not "increase" or "decrease".
+
+ Example Usage:
+
+   syscontrol.brightness_control("increase")
+   `This code will increase the brightness by 5%.`
+
+   syscontrol.brightness_control("decrease")
+   `This code will decrease the brightness by 5%.`
+
+   syscontrol.brightness_control("set", 50)
+   `This code will set the brightness to 50%.`
+]]
+---@param type string
+---@param value number
+---@return nil
 function syscontrol.brightness_control(type, value)
   local cmd
   if type == "increase" then
@@ -48,7 +108,7 @@ function syscontrol.brightness_control(type, value)
     cmd = "brightnessctl set -q " .. tostring(value) .. "%"
   end
 
-  spawn.easy_async(cmd, get_brightness)
+  spawn.easy_async(cmd, syscontrol.get_brightness)
 end
 
 -- ─────────────────────────────── Mic toggle ─────────────────────────────── --
@@ -61,7 +121,7 @@ function syscontrol.get_mic_mute()
 end
 
 function syscontrol.mic_toggle(type, value)
-  spawn.easy_async("pactl set-source-mute @DEFAULT_SOURCE@ toggle", get_mic_mute)
+  spawn.easy_async("pactl set-source-mute @DEFAULT_SOURCE@ toggle", syscontrol.get_mic_mute)
 end
 
 return syscontrol
