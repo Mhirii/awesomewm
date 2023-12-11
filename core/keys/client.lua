91bb6a9874c036294ed730a6a61dae82f89fda25
Mhirii <ahmedmhiri218@gmail.com>
2023-12-11 22:22:27 +0100

    refactor

--- core/keys/client.lua
+++ core/keys/client.lua
@@ -168,3 +168,21 @@ client.connect_signal(
     }
   end
 )
+
+client.connect_signal("request::default_mousebindings", function()
+  awful.mouse.append_client_mousebindings({ awful.button({}, 1, function(c)
+    c:activate {
+      context = "mouse_click"
+    }
+  end), awful.button({ modkey }, 1, function(c)
+    c:activate {
+      context = "mouse_click",
+      action = "mouse_move"
+    }
+  end), awful.button({ modkey }, 3, function(c)
+    c:activate {
+      context = "mouse_click",
+      action = "mouse_resize"
+    }
+  end) })
+end)
--- core/keys/layout.lua
+++ core/keys/layout.lua
@@ -161,21 +161,3 @@ awful.keyboard.append_global_keybindings({ awful.key {
     end
   end
 } })
-
-client.connect_signal("request::default_mousebindings", function()
-  awful.mouse.append_client_mousebindings({ awful.button({}, 1, function(c)
-    c:activate {
-      context = "mouse_click"
-    }
-  end), awful.button({ modkey }, 1, function(c)
-    c:activate {
-      context = "mouse_click",
-      action = "mouse_move"
-    }
-  end), awful.button({ modkey }, 3, function(c)
-    c:activate {
-      context = "mouse_click",
-      action = "mouse_resize"
-    }
-  end) })
-end)
