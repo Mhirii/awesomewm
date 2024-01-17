4888dfe4b3973b0950d6c97d59684dde0f7e22de
Mhirii <ahmedmhiri218@gmail.com>
2024-01-18 00:42:23 +0100

    wezterm

--- user_settings.lua
+++ user_settings.lua
@@ -2,10 +2,11 @@ local gears = require("gears")
 local config_dir = gears.filesystem.get_xdg_config_home()
 local scripts = config_dir .. "awesome/scripts/"
 
-local terminal = "alacritty --config-file " .. config_dir .. "awesome/conf/alacritty.toml"
+-- local terminal = "alacritty --config-file " .. config_dir .. "awesome/conf/alacritty.toml"
+local terminal = "wezterm"
 local editor = os.getenv("EDITOR") or "nvim"
 local editor_cmd = terminal .. " -e " .. editor
-local browser = os.getenv("BROWSER") or "firedragon"
+local browser = os.getenv("BROWSER") or "firefox"
 local dev_browser = os.getenv("DEV_BROWSER") or "firefox-developer-edition"
 local visual = "code --password-store='gnome'"
 local files = os.getenv("FILEBROWSER") or "ranger"
